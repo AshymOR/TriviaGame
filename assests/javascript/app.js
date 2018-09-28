2f12d0823664559ba7f6d869fff5c46c5618ecb0
Don <lordashym@gmail.com>
2018-09-27 21:24:54 -0500

    in progress

---
+++ assests/javascript/app.js
@@ -0,0 +1,35 @@
+var timer = 0;
+let choice = 0;
+
+$(document).ready(function() {
+    $('#optA').on('click', function() {
+      choice = "a";
+      $(function () {
+        $('#myModal').hide();
+     });
+    });
+    $('#optB').on('click', function() {
+        choice = "b";
+        
+      });
+      $('#optC').on('click', function() {
+        choice = "c";
+        
+      });
+      $('#optD').on('click', function() {
+        choice = "d";
+        
+      });
+
+
+  
+  
+  
+  
+});
+
+  
+
+    
+  
+  
