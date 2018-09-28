f96f19f7d751cd2dbcb5bb6d790fe3e07fb6c349
Don <lordashym@gmail.com>
2018-09-27 23:12:09 -0500

    deadline_incomplete

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -1,32 +1,49 @@
-var timer = 0;
-let choice = 0;
+var choice = 0
+
 
 $(document).ready(function() {
     $('#optA').on('click', function() {
-      choice = "a";
-      $(function () {
-        $('#myModal').hide();
-     });
+        choice = "a";
+        
+        $("#exampleModal").hide();
+        
+  
     });
-    $('#optB').on('click', function() {
-        choice = "b";
-        
-      });
-      $('#optC').on('click', function() {
-        choice = "c";
-        
-      });
-      $('#optD').on('click', function() {
-        choice = "d";
-        
-      });
+  });
 
 
+
+//get countdown timer working so every time the complex modal is shown it starts a 30 second countdown
+
+//finish the click handlers to get the players answer and set it to choice
+//get the other 9 questions set up
+
+
+//if time runs out show the modal with the times up template
+//Depending on what button is clicked set choice to either a, b, c, or d.
+//If choice === the correct answer make the simple modal with the *unfinished* correct template show increase score by 1
+//if choice !== the correct answer make the somple modal show with the incorrect template
+
+//have the simple modal show for 5 seconds before hiding
+//show complex modal with new question and answer options but using jquery to dynamically update the associated IDs
+
+//once all 10 questions are answered bring up the simple modal that displays score, percentage and a picture that
+//varies depending on what percent they got. Brackets being 0-59%, 60-79%, 80-99%, and 100%.
+
+//I'm disapointed I was not able to ge this done.I am not exactly sure how I need to procceed to get it working the way I want
+//it to. I'll be continuing to work on this during my free time and I'll be aking for help and tips.
+
+
+
+
+
+
+
   
   
   
   
-});
+
 
   
 
